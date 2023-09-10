9012a051ee758b80ad01f68a0f830ddfd9b362e4
= <=>
2023-09-10 15:55:15 +0700

    Method channel example Swift

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,48 @@
+import UIKit
+import Flutter
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    // Membuat event channel
+
+    let controller = window?.rootViewController as! FlutterViewController
+
+    let eventChannel = FlutterEventChannel(
+        name: "my_event_channel",  // Nama channel
+        binaryMessenger: controller.binaryMessenger
+    )
+    
+    // Menambahkan listener ke event channel
+    eventChannel.setStreamHandler(MyEventStreamHandler())
+
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+
+// StreamHandler untuk menangani peristiwa di event channel
+class MyEventStreamHandler: NSObject, FlutterStreamHandler {
+    static let shared = MyEventStreamHandler()
+    
+    private var eventSink: FlutterEventSink?
+    
+    func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        self.eventSink = events
+        sendEvent(data:"Hello")
+        return nil
+    }
+    
+    func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        self.eventSink = nil
+        return nil
+    }
+    
+    // Fungsi untuk mengirim peristiwa ke Flutter
+    func sendEvent(data: Any) {
+        eventSink?(data)
+    }
+}
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const platformChannel = EventChannel(
+      'my_event_channel'); // Nama channel sesuai dengan yang didefinisikan di Swift
+
+  String dataEvent = "";
+  @override
+  void initState() {
+    super.initState();
+    platformChannel.receiveBroadcastStream().listen((data) {
+      // Menghandle peristiwa yang diterima dari Swift
+
+      setState(() {
+        dataEvent = data.toString();
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Event Channel Example'),
+      ),
+      body: Center(
+        child: Text("Data dari Native : $dataEvent"),
+      ),
+    );
+  }
+}
